6289fe80cc2a78f106add8ee30e6f97709a091b8
Akshay Patil <akshaypatil.9837@gmail.com>
2019-09-29 01:34:26 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,70 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
+import { AuthService } from './auth.service';
+import { HomeComponent } from './home/home.component';
+import { CapitalizePipe } from './capitalize.pipe';
+import { AppRoutingModule } from './/app-routing.module';
+import { MenuComponent } from './menu/menu.component';
+import { MyblogsComponent } from './myblogs/myblogs.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CreateComponent } from './create/create.component';
+
+import { NgxEditorModule } from 'ngx-editor';
+import { HttpClientModule } from '@angular/common/http';
+import { PostComponent } from './post/post.component';
+import { ViewComponent } from './view/view.component';
+import { CommentsComponent } from './comments/comments.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+
+let firebaseConfig = {
+ 
+    apiKey: "AIzaSyCzh_m4xupu57Yl4MWQavccIC6K8dxdcyY",
+    authDomain: "blogento-32d54.firebaseapp.com",
+    databaseURL: "https://blogento-32d54.firebaseio.com",
+    projectId: "blogento-32d54",
+    storageBucket: "",
+    messagingSenderId: "825181253186",
+    appId: "1:825181253186:web:6632d9ebd212329fb0c7db",
+    measurementId: "G-74WKV88VMJ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    CapitalizePipe,
+    MenuComponent,
+    MyblogsComponent,
+    ProfileComponent,
+    CreateComponent,
+    PostComponent,
+    ViewComponent,
+    CommentsComponent,
+    EditProfileComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    NgxEditorModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
